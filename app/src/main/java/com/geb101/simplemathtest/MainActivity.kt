86297ddf2b1918951e4c212e920584527edefab2
Geb101 <georgestrel@gmail.com>
2024-03-26 20:47:08 +0300

    Added random generated math examples and fix grammar mistake

--- app/src/main/java/com/geb101/simplemathtest/MainActivity.kt
+++ app/src/main/java/com/geb101/simplemathtest/MainActivity.kt
@@ -8,25 +8,57 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private var number1 = randNumber()
+    private var number2 = randNumber()
+    private lateinit var textViewExample: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        savedInstanceState?.let {
+            number1 = it.getInt("number1")
+            number2 = it.getInt("number2")
+        }
+
+        textViewExample = findViewById(R.id.example)
         val editText = findViewById<TextView>(R.id.editTextNumbers)
         val button = findViewById<Button>(R.id.buttonAnswer)
         val correct = findViewById<TextView>(R.id.correctAnswer)
         val incorrect = findViewById<TextView>(R.id.incorrectAnswer)
+
+        setExample()
+
         button.setOnClickListener {
             val number = editText.text.toString()
-            if (number.equals("15")) {
-                correct.visibility = View.VISIBLE
-                incorrect.visibility = View.GONE
-            } else {
-                correct.visibility = View.GONE
-                incorrect.visibility = View.VISIBLE
-            }
+            val answer = number1 + number2
+                if (number == answer.toString()) {
+                    number1 = randNumber()
+                    number2 = randNumber()
+                    setExample()
+                    correct.visibility = View.VISIBLE
+                    incorrect.visibility = View.GONE
+                } else {
+                    correct.visibility = View.GONE
+                    incorrect.visibility = View.VISIBLE
+                }
         }
+    }
+    private fun randNumber(): Int {
+        return (0..100).random()
+    }
+    private fun setExample() {
+        textViewExample.text = "$number1 + $number2 = ?"
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("number1", number1)
+        outState.putInt("number2", number2)
     }
 }
